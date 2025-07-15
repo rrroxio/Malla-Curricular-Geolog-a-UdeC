9c6523b59f4802f5ba8bc2603e866e8b764b39f6
rrroxio <rocio.daniela.toledo@gmail.com>
2025-07-14 23:18:18 -0400

    script.js

--- script.js
+++ script.js
@@ -78,6 +78,8 @@ function actualizarDesbloqueos() {
     // Verificar si se cumplen prerrequisitos normales
     let puedeDesbloquear = reqs.every(r => aprobados.includes(r));
 
+  const puedeDesbloquear = reqs.length === 0 || reqs.every(r => aprobados.includes(r));
+    
 
     if (!elem.classList.contains('aprobado')) {
       if (puedeDesbloquear) elem.classList.remove('bloqueado');
